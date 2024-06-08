3562e35212940c24bff6596f9cde9b9c9c91a707
Hang Deng <79652724+bitEEdh@users.noreply.github.com>
2024-06-08 20:39:52 +0800

    Update start.py

--- start.py
+++ start.py
@@ -5,5 +5,5 @@ os.system(f'git clone https://code.openxlab.org.cn/OpenLMLab/internlm2-chat-1.8b
 os.system(f'cd {base_path} && git lfs pull')
 file_path = './files'
 os.system(f'git clone https://github.com/bitEEdh/LLM-assistant-for-Commun.git {file_path}')
-os.system(f'python RAG_init.py')
+os.system(f'python {file_path}/RAG_init.py')
 os.system('streamlit run web_demo.py --server.address=0.0.0.0 --server.port 7860')
