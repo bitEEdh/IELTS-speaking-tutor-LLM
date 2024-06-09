80dc86e817aa07e79a0df0a56810abe9b73f7504
Hang Deng <79652724+bitEEdh@users.noreply.github.com>
2024-06-09 16:21:33 +0800

    Update RAG_init.py

--- RAG_init.py
+++ RAG_init.py
@@ -6,16 +6,16 @@ from RAG.Embeddings import BgeEmbedding
 import os
 
 # 没有保存数据库
-if not os.path.isdir('/home/xlab-app-center/.cache/files/storage'):
-    docs = ReadFiles('/home/xlab-app-center/.cache/files/dataset').get_content(max_token_len=600, cover_content=150) # 获得data目录下的所有文件内容并分割
+if not os.path.isdir('/home/xlab-app-center/.cache/storage'):
+    docs = ReadFiles('dataset').get_content(max_token_len=600, cover_content=150) # 获得data目录下的所有文件内容并分割
     embedding = BgeEmbedding()
     vector = VectorStore(docs)
     vector.get_vector(EmbeddingModel=embedding)
-    vector.persist(path='/home/xlab-app-center/.cache/files/storage') # 将向量和文档内容保存到storage目录下，下次再用就可以直接加载本地的数据库
+    vector.persist(path='/home/xlab-app-center/.cache/storage') # 将向量和文档内容保存到storage目录下，下次再用就可以直接加载本地的数据库
 # 使用保存好的数据库
 else:
     vector = VectorStore()
-    vector.load_vector('/home/xlab-app-center/.cache/files/storage') # 加载本地的数据库
+    vector.load_vector('/home/xlab-app-center/.cache/storage') # 加载本地的数据库
     embedding = BgeEmbedding()
 
 # question = 'What is the reason for including sequence numbers in each packet encapsulated within a GRE-based Layer 3 overlay tunnel? Choose from the following options: A. To enable host mobility. B. To support Network Ingress Filtering. C. To prevent TCP collapse. D. To load-balance packets over different access networks. Before you choose an option, list the thinking process and give the reason of your choice.'
