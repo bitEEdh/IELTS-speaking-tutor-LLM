6de9d2edfc282b189ceaa5211b789da7fc6188e0
Hang Deng <79652724+bitEEdh@users.noreply.github.com>
2024-06-09 14:15:21 +0800

    Update start.py

--- start.py
+++ start.py
@@ -9,5 +9,5 @@ file_path = './files'
 os.system(f'cd ..')
 os.system(f'git clone https://github.com/bitEEdh/LLM-assistant-for-Commun.git {file_path}')
 os.system(f'python {file_path}/RAG_init.py')
-os.system(f'cd ..')
+os.system(f'cd {file_path}')
 os.system('streamlit run web_demo.py --server.address=0.0.0.0 --server.port 7860')
